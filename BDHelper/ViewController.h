d5512566d324da3c762f9f3afd7ff73a7ddc18c3
Mark_Mac <Mark_Mac@Mark-Mac.local>
2017-04-12 14:28:49 +0800

    上传项目文件

---
+++ BDHelper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BDHelper
+//
+//  Created by Mark_Mac on 2017/4/12.
+//  Copyright © 2017年 BD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BDHelper/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BDHelper
+//
+//  Created by Mark_Mac on 2017/4/12.
+//  Copyright © 2017年 BD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
